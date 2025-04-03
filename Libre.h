49eef9d45d6ae2715b8fe030455b78f2a0deabc5
GayGay <gaetanskateboard@gmail.com>
2025-04-03 07:33:10 +0200

    com

--- Libre.h
+++ Libre.h
@@ -7,12 +7,14 @@
 class Libre : public Contrainte
 {
     public :
+    // Aucun contrainte donc la force appliquée est simplement la somme des force extérieures
         virtual Vecteur applique_force(ObjetPhysique const& obj,  Vecteur const& force, double t) const override
         {
             if(obj.get_masse() != 0) return force*(1/obj.get_masse()); // a = F/m
             return force;
         }
 
+    // Aucune contrainte donc la position et la vitesse sont les vecteurs d'états originaux
         virtual Vecteur position(ObjetPhysique const& obj) const override
         { return obj.get_E(); }
 
